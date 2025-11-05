27c49c63e7a3cb1e19fa35b89a5212a005e5d425
am2308 <b110020510@gmail.com>
2025-11-05 09:06:17 +0000

    Comment out Route 53 zone resource
    Comment out the aws_route53_zone resource definition.

--- devops/modules/route53/main.tf
+++ devops/modules/route53/main.tf
@@ -1,9 +1,9 @@
-resource "aws_route53_zone" "skillsync" {
-  name = var.sub_domain_name
-  tags = merge(
-    var.default_tags
-  )
-}
+#resource "aws_route53_zone" "skillsync" {
+#  name = var.sub_domain_name
+#  tags = merge(
+#    var.default_tags
+#  )
+#}
 
 resource "aws_route53_record" "skillsync-ns" {
   zone_id = var.root_domain_zone_id
